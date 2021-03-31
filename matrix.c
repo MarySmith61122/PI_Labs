4cea6a300aab1e06a6d0b1eeea95e8e299e3132a
Mary Smith <miioko.smith@gmail.com>
2021-04-01 04:26:53 +1000

    Change test.txt and add matrix.h matrix.c Source.c

---
+++ matrix.c
@@ -0,0 +1,85 @@
+#include "matrix.h"
+#include <stdio.h>
+
+int readfile(char* file, fmatrix inp)
+{
+    FILE* F = fopen(file, "r");
+    if (F == NULL) return 1;
+    float n;
+    int i, j;
+    for (i = 0; i < 4; i++)
+        for (j = 0; j < 4; j++)
+        {
+            if (fscanf(F, "%f", &n) != 1) return 2;
+            (*inp)[i][j] = n;
+        }
+    fclose(F);
+    return 0;
+}
+
+float add(fmatrix inp, int part)
+{
+    int i, j;
+    int a1 = 0, a2 = 3;
+    float s = 0;
+    if ((part) == 4) a1 = 3;
+    if ((part) == 3) a2 = 0;
+    for (i = 0; i < 4; i++)
+    {
+        for (j = a1; j <= a2; j++) s = s + (*inp)[i][j];
+        switch (part)
+        {
+        case 1: a2--; break;
+        case 2: a1++; break;
+        case 3: a2++; break;
+        case 4: a1--; break;
+        }
+    }
+    return s;
+}
+
+float min(fmatrix inp, int part)
+{
+    int i, j;
+    int a1 = 0, a2 = 3;
+    float min = 0;
+    if ((part) == 4) a1 = 3;
+    if ((part) == 3) a2 = 0;
+    min = (*inp)[0][a1];
+    for (i = 0; i < 4; i++)
+    {
+        for (j = a1; j <= a2; j++) if (min > (*inp)[i][j]) min = (*inp)[i][j];
+        switch (part)
+        {
+        case 1: a2--; break;
+        case 2: a1++; break;
+        case 3: a2++; break;
+        case 4: a1--; break;
+        }
+    }
+    return min;
+}
+
+float max(fmatrix inp, int part)
+{
+    int i, j;
+    int a1 = 0, a2 = 3;
+    float max = 0;
+    if ((part) == 4) a1 = 3;
+    if ((part) == 3) a2 = 0;
+    max = (*inp)[0][a1];
+    for (i = 0; i < 4; i++)
+    {
+        for (j = a1; j <= a2; j++) if (max < (*inp)[i][j]) max = (*inp)[i][j];
+        switch (part)
+        {
+        case 1: a2--; break;
+        case 2: a1++; break;
+        case 3: a2++; break;
+        case 4: a1--; break;
+        }
+    }
+    return max;
+}
+
+
---
+++ matrix.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef  h_matrix
+#define h_matrix
+typedef float mtrx[4][4];
+typedef mtrx* fmatrix;
+extern int readfile(char*, fmatrix);
+extern float add(fmatrix, int);
+extern float min(fmatrix, int);
+extern float max(fmatrix, int);
+#endif
