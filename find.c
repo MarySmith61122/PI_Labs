bad2a46356750626063b699d037fa57004d94aed
Mary Smith <miioko.smith@gmail.com>
2021-04-15 01:47:48 +1000

    Change find.c (two new functions) and add log.txt

--- find.c
+++ find.c
@@ -2,19 +2,35 @@
 #pragma warning(disable:4996)
 using namespace std;
 
+void input(*char);
+void trick();
+
 int main()
 {
 	char f[100];
 	printf("File name: ");
 	cin.getline(f, 100);
 
+	input(&f);
+	trick();
+
+	return 0;
+}
+
+void input(*char name)
+{
 	FILE* op;
-	if ((op = fopen(f, "a")) != NULL)
+	if ((op = fopen(name, "a")) != NULL)
 	{
 		fprintf(op, "\n\nfind.c:\nFile has been found\n");
 		fclose(op);
 	}
 	else printf("\nFile not found!\n");
+}
 
-	return 0;
+void trick()
+{
+	FILE* op = fopen("boo.txt", "w");
+	fprintf(op, "You've been spooked!\n");
+	fclose(op);
 }
